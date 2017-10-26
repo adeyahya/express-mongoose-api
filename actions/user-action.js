3511ea0fde9e9c07955cc36d8fbb444245a6f549
Ade Yahya <adeyahyaprasetyo@gmail.com>
2017-10-26 11:16:43 +0700

    parse salt into number

--- actions/user-action.js
+++ actions/user-action.js
@@ -1,6 +1,6 @@
 const User = require("../models/User")
 const bcrypt = require("bcryptjs")
-const saltRounds = process.env.SALT_ROUNDS
+const saltRounds = Number(process.env.SALT_ROUNDS)
 
 exports.create = async function(obj) {
   // Hashing password
