65730dc7b79d082af1a336ee07b0453cb6ef475e
Ade Yahya <adeyahyaprasetyo@gmail.com>
2017-10-26 15:31:45 +0700

    upload test

--- __tests__/photos.test.js
+++ __tests__/photos.test.js
@@ -15,3 +15,25 @@ describe("Get all photos", () => {
     }
   })
 })
+
+describe("Upload photo", () => {
+  // changed jasmine timeout
+  beforeEach(function (done) {
+    window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    setTimeout(function () {
+      console.log('inside timeout');
+      done();
+    }, 500);
+  })
+
+  it("Should return 200 with photo", async () => {
+    try {
+      await request(app)
+        .post("/photos")
+        .attach("photo", "__tests__/dummy.jpg")
+        .expect(200)
+    } catch(e) {
+      throw new Error(e)
+    }
+  })
+})
